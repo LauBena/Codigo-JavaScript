cd4c29c5674b0b9ee058d76195311ab32a958a82
LauBena <laurabr314@gmail.com>
2022-02-22 16:26:51 -0300

    Vimos y trabajamos los distintos casos del Objeto Math - basicos

---
+++ ObjetoMath-Basico/codigo.js
@@ -0,0 +1,54 @@
+// METODOS
+let numero = Math.sqrt(25); // devuelve raiz cuadrada +
+document.write(numero + "<br>");
+
+numero = Math.cbrt(27); // devuelve raiz cubica
+document.write(numero + "<br>"); 
+
+
+numero = Math.max(25, 2, 18, 100, 90, 1, 28); // devuelve el num mas grande
+document.write(numero + "<br>");
+
+numero = Math.min(25,4, 1, 6, 32, 65, 0.2); // devuelve el num mas chico
+document.write(numero + "<br>");
+
+numero = Math.random(); // devuelve num aleatorio entre 0 y 1
+document.write(numero + "<br>");
+
+numero = Math.random()*100;
+numero = Math.round(numero); // redondea un num al num mas cercano.
+document.write(numero + "<br>");
+
+numero = Math.floor(numero); // devuelve el mayor entero menor que 0 o igual a un num. 
+document.write(numero + "<br>");
+
+numero = Math.fround(2.999999999999999999999999);
+document.write(numero + "<br>"); // devuelve la representacion flotante de precision simple mas cercana de un num
+
+numero = Math.trunc(9.8); // elimina los decimales.
+document.write(numero + "<br>");
+
+//PROPIEDADES
+numero = Math.PI;
+document.write(numero + "<br>");
+
+numero = Math.SQRT1_2; // raiz cuadrada de 1/2, representacion flotante al num mas cercano
+document.write(numero + "<br>");
+
+numero = Math.SQRT2; // raiz cuadrada de 2.
+document.write(numero + "<br>");
+
+numero = Math.E; // constante de euler, base de logaritmos naturales
+document.write(numero + "<br>");
+
+numero = Math.LN2; // logaritmo natural de 2
+document.write(numero + "<br>");
+
+numero = Math.LN10; // logaritmo natural de 10
+document.write(numero + "<br>");
+
+numero = Math.LOG2E; //logaritmo de E con base 2
+document.write(numero + "<br>");
+
+numero = Math.LOG10E; // logaritmo de E con base 10
+document.write(numero + "<br>");

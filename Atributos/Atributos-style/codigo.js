5d3689d7b535bb13d3033d7f44ccb8442a8c6ee4
LauBena <laurabr314@gmail.com>
2022-03-08 19:58:42 -0300

    aplicamos atributos style para modificar nuestro html desde js

---
+++ Atributos/Atributos-style/codigo.js
@@ -0,0 +1,6 @@
+const titulo = document.querySelector(".titulo");
+
+titulo.style.color = "pink";
+titulo.style.backgroundColor = "black";
+
+// utilizados para modificar el estilo de nuestro html desde JS

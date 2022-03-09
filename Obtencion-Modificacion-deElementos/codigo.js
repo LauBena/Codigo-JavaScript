d19776b060e162d03d9d981d247d5277915e07ee
LauBena <laurabr314@gmail.com>
2022-03-08 23:39:15 -0300

    trabajamos en nuestro archivo .js los diferentes tipos de obtencion y modificacion de elementos

---
+++ Obtencion-Modificacion-deElementos/codigo.js
@@ -0,0 +1,10 @@
+const titulo = document.querySelector(".titulo");
+
+let resultado = titulo.textContent; // Devuelve el texto de cualquier nodo.
+alert(resultado);
+
+let result = titulo.innerHTML; // Devuelve el contenido html de un elemento.
+alert(result);
+
+let resulta2 = titulo.outerHTML; // Devuelve el codigo HTML completo del elemento.
+alert(resulta2);
